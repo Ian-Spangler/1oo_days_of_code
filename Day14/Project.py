70df77bade6e45b88c83030a618952230cf2aa1d
Ian-Spangler <e1070578@u.nus.edu>
2025-03-09 10:25:19 +0000

    Day14

--- Day14/Project.py
+++ Day14/Project.py
@@ -1,5 +1,8 @@
 # Higer or Lower
 # url = https://appbrewery.github.io/python-day14-demo/
+import random
+import game_data
+
 higher_lower_logo = '''
     __  ___       __             
    / / / (_)___ _/ /_  ___  _____
@@ -9,8 +12,7 @@ higher_lower_logo = '''
    / /  /____/_      _____  _____
   / /   / __ \ | /| / / _ \/ ___/
  / /___/ /_/ / |/ |/ /  __/ /    
-/_____/\____/|__/|__/\___/_/     
-                                 
+/_____/\____/|__/|__/\___/_/                                 
 '''
 vs_logo = '''
 ____   ____      
@@ -20,16 +22,50 @@ ____   ____
    \___//____  > 
              \/  
 '''
+
+data_center = game_data.data
+score = 0
+play = True
+
+def selecter(dic):
+    choice = random.choice(dic)
+    return choice
+
+def format(block, str):
+    print(f"Compare {str}: {block["name"]}, a {block["description"]}, from {block["country"]}.")
+
 def compare(a, b):
-    if a == b:
+    a_follower = a["follower_count"]
+    b_follower = b["follower_count"]
+    if a_follower == b_follower:
         return 0
-    elif a > b:
-        return 1
+    elif a_follower > b_follower:
+        return "A"
     else:
-        return -1
-    
-print("You're right! Current score: 1")
-print("Compare A: Shakira, a Musician, from Colombia")
-print("Against B: Zendaya, a Actress and musician, from USA")
-print("Sorry, that's wrong. Final score: 0")
+        return "B"
+
+choice_1 = selecter(data_center)
+print(higher_lower_logo)
+
+while play:
+    choice_2 = selecter(data_center)
+    if choice_1 == choice_2:
+        choice_2 = selecter(data_center)
+    format(choice_1, "A")
+    print(vs_logo)
+    format(choice_2, "B")
+    result = compare(choice_1, choice_2)
+    user_choice = input("Who has more followers? Type 'A' or 'B': ")
+    if result == user_choice:
+        score += 1
+        print("\n" *20)
+        print(higher_lower_logo)
+        print(f"You're right! Current score: {score}")
+        if result == 'B':
+            choice_1 = choice_2
+    else:
+        print("\n" *20)
+        print(higher_lower_logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        play = False
 
