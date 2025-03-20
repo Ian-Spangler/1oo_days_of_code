55105f69fd7b5bed87050b79cf3d9d0b6d13bc30
Ian-Spangler <e1070578@u.nus.edu>
2025-03-20 11:47:01 +0000

    Day18

--- Day18/main.py
+++ Day18/main.py
@@ -9,17 +9,26 @@ timmy_the_turtle = Turtle()
 # timmy_the_turtle.forward(100)
 # timmy_the_turtle.right(90)
 
-# draw a square
-for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# # draw a square
+# for i in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+#
+# # draw a dashed line
+# for i in range(15):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
 
-# draw a dashed line
-for i in range(15):
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.pendown()
+# # draw different shapes
+# for i in range(3, 9):
+#     for j in range(i):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(360/i)
+
+# draw random walk
+
 
 screen = Screen()
 screen.exitonclick()
