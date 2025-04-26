c1da452df0f9cea53a8a90278799a340c3c4a3e2
Ian-Spangler <e1070578@u.nus.edu>
2025-04-26 14:08:15 +0000

    Day46

--- Day46/main.py
+++ Day46/main.py
@@ -1,6 +1,11 @@
 # Creating Spotify Playlist using the Musical Time Machine
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv
+
+SPOTIFY_CLIENT_ID = os.environ["CLIENT_ID"]
+SPOTIFY_CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
 BILLBOARD_URL = "https://www.billboard.com/charts/hot-100/"
 
@@ -9,12 +14,16 @@ date = input("Which year do you want to travel to? Type the data in this format 
 bilboard_header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
 
-response = requests.get(url=f"{BILLBOARD_URL}{date}/", headers=bilboard_header)
+bilboard_response = requests.get(url=f"{BILLBOARD_URL}{date}/", headers=bilboard_header)
 
-bilboard_webpage = response.text
+bilboard_webpage = bilboard_response.text
 
 soup = BeautifulSoup(bilboard_webpage, "html.parser")
 
 top_list = soup.select("li ul li h3")
 
 music_titles = [item.getText().strip() for item in top_list]
+
+spotify_header = {
+    
+}
