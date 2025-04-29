ed8bf77131141c9f79ef04700da42291693ce719
Ian-Spangler <e1070578@u.nus.edu>
2025-04-29 11:13:48 +0000

    Day48

---
+++ Day48/main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction").text
+
+print(f"The price is {price_dollar}.{price_cents}")
+
+# driver.close() # Close the active tab
+driver.quit() # Close the entire browser (all tabs)
