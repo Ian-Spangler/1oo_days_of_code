006f823c1624dae9971a79f93cc829f05334b3ba
Ian-Spangler <e1070578@u.nus.edu>
2025-05-18 11:20:02 +0000

    Day63

--- Day63/Project/main.py
+++ Day63/Project/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -15,11 +18,30 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
-all_books = []
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        db.session.delete(user)
+        db.session.commit()
     return render_template('index.html', books=all_books)
 
 
@@ -27,15 +49,17 @@ def home():
 def add():
     if request.method == "POST":
         data = request.form
-        book = {
-            "title": data["title"],
-            "author": data["author"],
-            "rating": data["rating"]
-        }
-        all_books.append(book)
-        print(all_books)
+        with app.app_context():
+            new_book = Book(id=data["id"], title=data["title"], author=data["author"], rating=data["rating"])
+            db.session.add(new_book)
+            db.session.commit()
+        return render_template('index.html')
     return render_template('add.html')
 
+@app.route("/edit/id=<int:number>")
+def edit():
+    
+    return render_template('edit.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
