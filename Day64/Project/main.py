4caf30cf022d7a192db2737fc4ec1667b0cd6651
Ian-Spangler <e1070578@u.nus.edu>
2025-05-23 11:23:45 +0000

    Day64

--- Day64/Project/main.py
+++ Day64/Project/main.py
@@ -21,6 +21,11 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+API_KEY = "01e046907607b949fcbbb839223cd7b9"
+MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_URL = "https://api.themoviedb.org/3/movie/"
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
@@ -47,20 +52,27 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
-class MyForm(FlaskForm):
+class EditForm(FlaskForm):
     rating = StringField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
     review = StringField('Your Review', validators=[DataRequired()])
     submit = SubmitField('Done')
 
+class AddForm(FlaskForm):
+    rating = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
 @app.route("/")
 def home():
-    result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
     all_movies = result.scalars().all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
-    form = MyForm()
+    form = EditForm()
     if request.method == "POST":
         movie_id = request.args.get("id")
         movie_to_update = db.get_or_404(Movie, movie_id)
@@ -69,6 +81,51 @@ def edit():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("edit.html", form=form)
+
+@app.route("/delete")
+def delete():
+    movie_id = request.args.get("id")
+    movie_to_delete = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route("/add")
+def add():
+    form = AddForm()
+    if form.validate_on_submit:
+        new_movie_title = form.title.data
+        movie_paramas = {
+            "api_key": API_KEY,
+            "query": new_movie_title
+        }
+
+        response = requests.get(url=MOVIE_SEARCH_URL, params=movie_paramas)
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+@app.route("/find")
+def find_movie():
+    id = request.args.get("id")
+    if id:
+        movie_detail_paramas = {
+            "api_key": API_KEY,
+            "language": 'en-US'
+        }
+        response = requests.get(url=f"{MOVIE_URL}/{id}", params=movie_detail_paramas)
+        data = response.json()
+        new_movie = Movie(
+            title = data["title"],
+            year = data["release_date"].split("-")[0],
+            img_url = f"{MOVIE_URL}/{id}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('edit'), id=new_movie.id)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
